70edd503194289cdffbfd3e5d913913add616d43
Will Marshall <will.marshall1@avant.com>
2023-06-03 15:59:32 -0400

    Changes

--- nn.py
+++ nn.py
@@ -4,7 +4,7 @@ import pandas as pd
 lookup = dict(K=1_000, M=1_000_000, B=1_000_000_000)
 LEARNING_RATE = 0.001
 
-df = pd.read_csv('/home/will/Downloads/BTC-USD.csv')
+df = pd.read_csv('./BTC-USD.csv')
 pv = df[['Close', 'Volume']]
 pv = pv.to_numpy()
 
@@ -58,6 +58,7 @@ test_X = test_X_flat
 W_sizes = [
     (DPOINT_DIMENSION * LOOKBACK, DPOINT_DIMENSION * LOOKBACK),
     (DPOINT_DIMENSION * LOOKBACK, DPOINT_DIMENSION * LOOKBACK),
+    (DPOINT_DIMENSION * LOOKBACK, DPOINT_DIMENSION * LOOKBACK),
     (DPOINT_DIMENSION * LOOKBACK, 1)
 ]
 
@@ -78,46 +79,58 @@ def feed_forward(_input):
             # activation = sigmoid(activation @ Ws[i] + bs[i])
     return activation
 
+j = 0
 
 def train():
     yhat = feed_forward(train_X)
     prev_error = sum((train_y - yhat)**2)[0]
-
-    j = 0
+    global j
     while True:
         # d_Ws = [1 + (np.random.randint(2, size=x)*2 - 1) * LEARNING_RATE for x in W_sizes]
         # d_bs = [1 + (np.random.randint(2, size=(1,x[1]))*2 - 1) * LEARNING_RATE for x in W_sizes]
-        d_Ws = [(np.random.randint(3, size=x) - 1) * LEARNING_RATE for x in W_sizes]
-        d_bs = [(np.random.randint(3, size=(1,x[1])) - 1) * LEARNING_RATE for x in W_sizes]
+        d_Ws = [(np.random.randint(2, size=x)*2 - 1) * LEARNING_RATE for x in W_sizes]
+        d_bs = [(np.random.randint(2, size=(1,x[1]))*2 - 1) * LEARNING_RATE for x in W_sizes]
 
         for i in range(len(W_sizes)):
             Ws[i] += d_Ws[i]
             bs[i] += d_bs[i]
+        try:
 
-        yhat = feed_forward(train_X)
-        error = sum((train_y - yhat)**2)[0]
+            yhat = feed_forward(train_X)
+            error = sum((train_y - yhat)**2)[0]
 
-        if error < prev_error:
-            # test_yhat = feed_forward(test_X)
-            # test_error = sum((test_y - test_yhat)**2)[0]
-            for x,y,yhat in zip(train_X[-10:], train_y[-10:], yhat[-10:]):
-                p_x = ', '.join([f'{_x:5f}' for _x in x])
-                p_y = ', '.join([f'{_y:5f}' for _y in y])
-                p_yhat = ', '.join([f'{_yhat:5f}' for _yhat in yhat])
-                print(p_x, ' --> ', p_y, f'(guess: {p_yhat})')
-            print()
-            for w in Ws:
-                print(w)
+            if error < prev_error:
+                # test_yhat = feed_forward(test_X)
+                # test_error = sum((test_y - test_yhat)**2)[0]
+                for x,y,yhat in zip(train_X[-10:], train_y[-10:], yhat[-10:]):
+                    p_x = ', '.join([f'{_x:5f}' for _x in x])
+                    p_y = ', '.join([f'{_y:5f}' for _y in y])
+                    p_yhat = ', '.join([f'{_yhat:5f}' for _yhat in yhat])
+                    print(p_x, ' --> ', p_y, f'(guess: {p_yhat})')
                 print()
-            print(f'TRAINING_ERROR({j}): {error}')
-            print()
-            # print(f'TRAINING_ERROR: {error}     TESTING_ERROR: {test_error}')
-            prev_error = error
-            continue
+                for w in Ws:
+                    print(w)
+                    print()
+                test_yhat = feed_forward(test_X)
+                test_error = sum((test_y - test_yhat)**2)[0]
+                print(f'TRAINING_ERROR({j}): {error}', f'LEARNING RATE: {LEARNING_RATE}')
+                print(f'TESTING_ERROR: {test_error}')
+                print()
+
+                prev_error = error
+                print(f'sum at the bottom {sum([sum(x.flatten()) for x in Ws])}')
+                continue
+        except:
+            print('in except')
+            for i in range(len(W_sizes)):
+                Ws[i] -= d_Ws[i]
+                bs[i] -= d_bs[i]
+            raise
 
         for i in range(len(W_sizes)):
             Ws[i] -= d_Ws[i]
             bs[i] -= d_bs[i]
+
         j += 1
 
 train()
